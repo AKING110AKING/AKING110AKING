e4badb359ec51b62e4266b4a5dbdf6bd146dd96c
AKING110AKING <111263808+AKING110AKING@users.noreply.github.com>
2022-08-15 10:18:51 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,37 +1,12 @@
 import os,time,platform 
- print('\n\x1b[1;37m[•] Checking Update...');time.sleep(0.5) 
+ os.system('clear') 
+ print('[•] Checking Updates...') 
  os.system('git pull') 
- os.system('xdg-open https://www.facebook.com/bhatti4055/permalink/374959374594682/') 
- logo = ("""\033[1;37m    ###    ##    ## #### ##    ##  ######  ❤ 
-    ## ##   ##   ##   ##  ###   ## ##    ## 
-   ##   ##  ##  ##    ##  ####  ## ## 
-  ##     ## #####     ##  ## ## ## ##   #### 
-  ######### ##  ##    ##  ##  #### ##    ## 
-  ##     ## ##   ##   ##  ##   ### ##    ## 
-  ##     ## ##    ## #### ##    ##  ######  ❤ 
- (!)══════════════════════════════════════════ 
- (!) Author   : IMTIAZ AKING 
- (!) Guthub   : AKING110 
- (!) Facebook : MR.AKING.07 
- (!) Type     : Free 
- (!) Version  : 1.2.2 
- \033[1;37m(!)══════════════════════════════════════════""") 
-  
- def Run(): 
-         bit = platform.architecture()[0] 
-         os.system('clear') 
-         print(logo) 
-         print('[•] Choose Your Country For Cloning\n\033[1;37m(!)══════════════════════════════════════════') 
-         print('[1] Pak Cloning \n[2] BD Cloning\n[0] Exit') 
-         Aking = input('[•] Choose : ') 
-         if Aking =='1': 
-                 if bit =='32bit': 
-                         import AKING32 
-                 elif bit =='64bit': 
-                         import AKING 
-         elif Aking =='2': 
-                 if bit =='32bit': 
-                         import BD32 
-                 elif bit =='64bit': 
-                         import BD 
- Run()
+ print('<------------------------------------>') 
+ bit = platform.architecture()[0] 
+ if bit=='32bit': 
+     print('[•] Join Over Facebook Group For Any Help') 
+     os.system('xdg-open https://facebook.com/groups/351076900316263/') 
+     import AKING 
+ else: 
+     print('\033[1;31m[×] Sorry Device Not Support'
